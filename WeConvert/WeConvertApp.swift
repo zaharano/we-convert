3b5fe189525695ee708e1ce90ae40deb30de94b7
Zach <zraisnorman@gmail.com>
2024-08-13 11:00:03 -0400

    Initial Commit

---
+++ WeConvert/WeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeConvertApp.swift
+//  WeConvert
+//
+//  Created by Zachary Rais-Norman on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
